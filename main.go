d2fa160342e610753a67b112e945f2398b4b9176
Marius <mariusdieckmann90@gmail.com>
2020-12-14 15:35:21 +0100

    added working main function

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/ag-computational-bio/bakta-web-backend/endpoints"
+)
+
+func main() {
+	err := endpoints.RunGrpcJobServer()
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
 }
