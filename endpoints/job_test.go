dfc5deca2a537159086a689d887fe06385be6d2a
Sebastian Beyvers <sb@pus.de>
2021-06-02 12:23:21 +0200

    Added docker-compose.test.yml for integration tests.

---
+++ endpoints/job_test.go
@@ -0,0 +1,19 @@
+package endpoints
+
+import "testing"
+
+func TestBaktaJobAPI_InitJob(t *testing.T) {
+
+}
+
+func TestBaktaJobAPI_StartJob(t *testing.T) {
+
+}
+
+func TestBaktaJobAPI_JobsStatus(t *testing.T) {
+
+}
+
+func TestBaktaJobAPI_JobResult(t *testing.T) {
+
+}
